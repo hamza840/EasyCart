4172ae07ed561bffb41a1b9f1926b1a89d45c8dc
Hamza Ahmed <hamza.ahmed@telenorbank.pk>
2019-10-08 15:56:48 +0500

    updated schema

--- easycart.sql
+++ easycart.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 03, 2019 at 01:53 PM
+-- Generation Time: Oct 08, 2019 at 12:55 PM
 -- Server version: 10.4.6-MariaDB
 -- PHP Version: 7.1.32
 
@@ -67,6 +67,49 @@ INSERT INTO `category` (`id`, `name`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `comment`
+--
+
+CREATE TABLE `comment` (
+  `id` int(11) NOT NULL,
+  `comment_detail` varchar(100) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `comment`
+--
+
+INSERT INTO `comment` (`id`, `comment_detail`, `product_id`, `user_id`) VALUES
+(1, 'best mobile phone', 3, 1),
+(2, 'need to fix some bugs', 3, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `icon`
+--
+
+CREATE TABLE `icon` (
+  `id` int(11) NOT NULL,
+  `small` varchar(100) NOT NULL,
+  `medium` varchar(100) NOT NULL,
+  `large` varchar(100) NOT NULL,
+  `xlarge` varchar(100) NOT NULL,
+  `xxlarge` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `icon`
+--
+
+INSERT INTO `icon` (`id`, `small`, `medium`, `large`, `xlarge`, `xxlarge`) VALUES
+(1, 'images/small.jpg', 'images/medium.jpg', 'images/large.jpg', 'images/xlarge.jpg', 'images/xxlarge.jpg');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `order_`
 --
 
@@ -108,21 +151,105 @@ CREATE TABLE `payment` (
 
 CREATE TABLE `product` (
   `id` int(11) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `category_id` int(11) NOT NULL,
-  `img_src` varchar(200) NOT NULL,
-  `price` bigint(20) NOT NULL,
-  `description` varchar(500) NOT NULL
+  `product_name` varchar(30) NOT NULL,
+  `parent_id` int(11) DEFAULT NULL,
+  `description` varchar(100) DEFAULT NULL,
+  `type_id` int(11) NOT NULL,
+  `icon_id` int(11) DEFAULT NULL,
+  `promotion_id` int(11) DEFAULT NULL,
+  `total_products` int(11) DEFAULT NULL,
+  `in_stock_products` int(11) DEFAULT NULL,
+  `price` bigint(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `product`
 --
 
-INSERT INTO `product` (`id`, `name`, `category_id`, `img_src`, `price`, `description`) VALUES
-(2, 'iphone11', 1, 'images/iphone11.png', 350000, 'Iphone 11 is the latest product from apple'),
-(4, 'TShirt', 2, 'images/shirt.png', 650, 'Imported shirt'),
-(5, 'Jeans', 2, 'images/shirt.png', 950, 'Imported jeans');
+INSERT INTO `product` (`id`, `product_name`, `parent_id`, `description`, `type_id`, `icon_id`, `promotion_id`, `total_products`, `in_stock_products`, `price`) VALUES
+(1, 'electronics', NULL, NULL, 2, NULL, NULL, NULL, NULL, NULL),
+(2, 'mobile phones', 1, NULL, 3, NULL, NULL, NULL, NULL, NULL),
+(3, 'iphone 8', 2, 'best selling mobile phone available in the market', 1, 1, 1, 100, 100, 80000),
+(5, 'home appliances', 1, NULL, 3, NULL, NULL, NULL, NULL, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_rating`
+--
+
+CREATE TABLE `product_rating` (
+  `id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `rating` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `promotion`
+--
+
+CREATE TABLE `promotion` (
+  `id` int(11) NOT NULL,
+  `promotion_name` varchar(30) NOT NULL,
+  `percentage` int(11) NOT NULL,
+  `start_date` timestamp NULL DEFAULT NULL,
+  `end_date` timestamp NULL DEFAULT NULL,
+  `created_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_date` timestamp NULL DEFAULT NULL,
+  `deleted_date` timestamp NULL DEFAULT NULL,
+  `status` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `promotion`
+--
+
+INSERT INTO `promotion` (`id`, `promotion_name`, `percentage`, `start_date`, `end_date`, `created_date`, `updated_date`, `deleted_date`, `status`) VALUES
+(1, 'summer sale', 40, '2019-10-08 19:00:00', '2019-10-15 19:00:00', '2019-10-08 06:45:15', NULL, NULL, 'inactive');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ratting`
+--
+
+CREATE TABLE `ratting` (
+  `id` int(11) NOT NULL,
+  `rate` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `ratting`
+--
+
+INSERT INTO `ratting` (`id`, `rate`, `product_id`, `user_id`) VALUES
+(1, 5, 3, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `type`
+--
+
+CREATE TABLE `type` (
+  `id` int(11) NOT NULL,
+  `type_name` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `type`
+--
+
+INSERT INTO `type` (`id`, `type_name`) VALUES
+(1, 'product'),
+(2, 'category'),
+(3, 'sub_category'),
+(4, 'domain');
 
 -- --------------------------------------------------------
 
@@ -139,6 +266,13 @@ CREATE TABLE `user` (
   `address` varchar(100) NOT NULL,
   `password` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `first_name`, `last_name`, `email`, `number`, `address`, `password`) VALUES
+(1, 'ayaz', 'ali', 'ayaz@gmail.com', '03248557483', 'rawalpindi', '786');
 
 --
 -- Indexes for dumped tables
@@ -161,6 +295,20 @@ ALTER TABLE `cart`
 -- Indexes for table `category`
 --
 ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `comment`
+--
+ALTER TABLE `comment`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `product_id` (`product_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `icon`
+--
+ALTER TABLE `icon`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -190,7 +338,38 @@ ALTER TABLE `payment`
 --
 ALTER TABLE `product`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `category_id` (`category_id`);
+  ADD KEY `icon_id` (`icon_id`),
+  ADD KEY `parent_id` (`parent_id`),
+  ADD KEY `promotion_id` (`promotion_id`),
+  ADD KEY `type_id` (`type_id`);
+
+--
+-- Indexes for table `product_rating`
+--
+ALTER TABLE `product_rating`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `product_id` (`product_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `promotion`
+--
+ALTER TABLE `promotion`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ratting`
+--
+ALTER TABLE `ratting`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `product_id` (`product_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `type`
+--
+ALTER TABLE `type`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `user`
@@ -213,6 +392,18 @@ ALTER TABLE `admin`
 --
 ALTER TABLE `category`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `comment`
+--
+ALTER TABLE `comment`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `icon`
+--
+ALTER TABLE `icon`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `order_`
@@ -239,10 +430,34 @@ ALTER TABLE `product`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
+-- AUTO_INCREMENT for table `product_rating`
+--
+ALTER TABLE `product_rating`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `promotion`
+--
+ALTER TABLE `promotion`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `ratting`
+--
+ALTER TABLE `ratting`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `type`
+--
+ALTER TABLE `type`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Constraints for dumped tables
@@ -254,6 +469,13 @@ ALTER TABLE `user`
 ALTER TABLE `cart`
   ADD CONSTRAINT `cart_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`),
   ADD CONSTRAINT `cart_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+
+--
+-- Constraints for table `comment`
+--
+ALTER TABLE `comment`
+  ADD CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`),
+  ADD CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
 
 --
 -- Constraints for table `order_`
@@ -273,7 +495,24 @@ ALTER TABLE `order_detail`
 -- Constraints for table `product`
 --
 ALTER TABLE `product`
-  ADD CONSTRAINT `product_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`);
+  ADD CONSTRAINT `product_ibfk_2` FOREIGN KEY (`icon_id`) REFERENCES `icon` (`id`),
+  ADD CONSTRAINT `product_ibfk_3` FOREIGN KEY (`parent_id`) REFERENCES `product` (`id`),
+  ADD CONSTRAINT `product_ibfk_4` FOREIGN KEY (`promotion_id`) REFERENCES `promotion` (`id`),
+  ADD CONSTRAINT `product_ibfk_5` FOREIGN KEY (`type_id`) REFERENCES `type` (`id`);
+
+--
+-- Constraints for table `product_rating`
+--
+ALTER TABLE `product_rating`
+  ADD CONSTRAINT `product_rating_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`),
+  ADD CONSTRAINT `product_rating_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+
+--
+-- Constraints for table `ratting`
+--
+ALTER TABLE `ratting`
+  ADD CONSTRAINT `ratting_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`),
+  ADD CONSTRAINT `ratting_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
---
+++ src/main/java/com/org/telenor/crudservice/config/SwaggerConfig.java
@@ -0,0 +1,4 @@
+package com.org.telenor.crudservice.config;
+
+public class SwaggerConfig {
+}
